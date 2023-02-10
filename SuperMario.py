8dcc2e9bc915a0f2adc6f3a2ad5c0ff67b19b3b8
Jocke <jocke17h@hotmail.com>
2023-02-10 07:25:56 +0100

    Getch() and single row print
    Using Getch() prevents echoing and return character with byte prefix
    Now prints everything on a single line

--- SuperMario.py
+++ SuperMario.py
@@ -9,19 +9,21 @@ letterList = ["x"]
 printCharacter = ""
 
 while True:    
-    if  msvcrt.getwche() == "d":  
-        letterList.insert(0," ") 
-        print("\nInserting value")
+    movementInput = msvcrt.getch() 
+    if  movementInput == b'd':  
+        letterList.insert(0,"") 
+        #print("Inserting value")
         #If list is NOT empty
-    if  msvcrt.getwche() == "a" and letterList:
+    if  movementInput == b'a' and letterList:
+        #print("Popping value")
         letterList.pop(0)
-        print("\nPopping value")
         #If list IS empty
-    elif msvcrt.getwche() == "a" and not letterList:
-        print("\nNot Full")
+    elif movementInput == b'a' and not letterList:
+        print("Not Full")
      
     for x in letterList:
         printCharacter += x
-        print("\nLetters in charlist" + printCharacter) 
+        #"Letters in charlist"
+        print(printCharacter, end="") 
          
     printCharacter = "" 
