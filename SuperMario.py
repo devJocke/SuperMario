f645bec9731b942a6d3d43149cfdbc967ead3fda
Jocke <jocke17h@hotmail.com>
2023-02-13 13:01:34 +0100

    Left and right movement
    Basic left and right added, caret placement added \r to flush the row each time its printed.

--- SuperMario.py
+++ SuperMario.py
@@ -3,27 +3,32 @@ import msvcrt
 # Todo:  Need input specific for unix and mac 
  
 
-print("SuperBario")
+print("SuperBario\n")
 
-letterList = ["x"]
-printCharacter = ""
+playerPositions = ["x"] 
+#Prints the first character X then sets the caret at the same position instead of jumping forward 
+print(playerPositions[0],end="\r") 
 
 while True:    
+
     movementInput = msvcrt.getch() 
+
     if  movementInput == b'd':  
-        letterList.insert(0,"") 
-        #print("Inserting value")
+        playerPositions.insert(0," ") 
+
         #If list is NOT empty
-    if  movementInput == b'a' and letterList:
-        #print("Popping value")
-        letterList.pop(0)
-        #If list IS empty
-    elif movementInput == b'a' and not letterList:
-        print("Not Full")
-     
-    for x in letterList:
-        printCharacter += x
-        #"Letters in charlist"
-        print(printCharacter, end="") 
-         
-    printCharacter = "" 
+    if  movementInput == b'a' and playerPositions: 
+
+        #Make sure position does not include X(that would mean we are at the left edge of the map)
+        if playerPositions[0] != "x":
+        #In order to move X we add a space to the end and pop the first one
+         playerPositions.append(" ")
+         playerPositions.pop(0)
+        #If list IS empty should never happen
+    elif movementInput == b'a' and not playerPositions:
+        print("Empty")
+   
+    for position in playerPositions: 
+        print(position, end="")
+
+    print(end="\r")
